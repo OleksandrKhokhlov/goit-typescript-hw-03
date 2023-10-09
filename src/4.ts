6d716eb6251f4990287270b83862786ba2c39588
OleksandrKhokhlov <hohlovsanya@gmail.com>
2023-10-09 22:29:01 +0300

    HW3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,55 @@
+class Key {
+  constructor(protected signature: number = Math.random()) {}
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Entered the house");
+    } else {
+      console.log("The door is closed. Cannot enter.");
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("Invalid key. Cannot open the door.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
+
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
